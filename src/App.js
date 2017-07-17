2842cd9449e5718ae21777c88f7a4b750a599888
Jay Nicholas Jackson <jaynickjackson@gmail.com>
2017-07-17 13:04:06 -0400

    Add title, tab icon, and logo

--- src/App.js
+++ src/App.js
@@ -1,18 +1,18 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+
 import './App.css';
+import chef from './icons/chef.png'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <h1>Bustle Baker</h1>
+        <img 
+          className='logo'
+          src={chef}
+          alt='Chef Logo'
+        />
       </div>
     );
   }
